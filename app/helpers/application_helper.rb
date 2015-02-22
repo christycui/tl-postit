0c50874bab36d3352eebb74473e03d87b7c952a6
christycui <christycui14@gmail.com>
2015-02-22 16:24:53 -0500

    added time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-  def format_time(time)
-    time.strftime("%A, %b %-d, %Y")
+  def format_time(dt)
+    if logged_in? && !current_user.time_zone.blank?
+      dt = dt.in_time_zone(current_user.time_zone)
+    end
+    dt.strftime("%A, %b %-d, %Y %l:%M%P %Z")
   end
 end
