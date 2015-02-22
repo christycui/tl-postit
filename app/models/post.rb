ef10d7231721ffe5bccf77a32f823c8eb2097fad
christycui <christycui14@gmail.com>
2015-02-21 19:41:10 -0500

    added slug to users and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,30 @@ class Post < ActiveRecord::Base
 
 
   def generate_slug
-    self.slug = self.title.gsub(' ','-').downcase
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 1
+    while post && post != self
+      count += 1
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def to_slug(name)
+    str = title.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
   end
 
   def to_param
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,37 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: {minimum:3}, uniqueness: true
   validates :password, presence: true, length: {minimum:6}, on: :create
 
+  before_save :generate_slug
+
+
+  def generate_slug
+    the_slug = to_slug(self.title)
+    user = User.find_by slug: the_slug
+    count = 1
+    while user && user != self
+      count += 1
+      the_slug = append_suffix(the_slug, count)
+      user = User.find_by slug: the_slug
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def to_slug(name)
+    str = username.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_param
+    self.slug
+  end
 end
